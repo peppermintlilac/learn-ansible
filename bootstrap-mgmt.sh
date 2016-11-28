22d99b5d6aa1022ea9ac22282e809d89430f8bf8
Kerr <E171612@twcable.com>
2016-11-28 11:59:59 -0500

    First commit

---
+++ bootstrap-mgmt.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# install ansible (http://docs.ansible.com/intro_installation.html)
+apt-get -y install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get -y install ansible
+
+# copy examples into /home/vagrant (from inside the mgmt node)
+cp -a /vagrant/examples/* /home/vagrant
+chown -R vagrant:vagrant /home/vagrant
+ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N ''
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa*
+chmod 600 /home/vagrant/.ssh/id_rsa
+#cp /vagrant/mgmt.privatekey .ssh/id_rsa
+#chmod 600 .ssh/id_rsa
+#chown vagrant:vagrant .ssh/id_rsa
+
+# configure hosts file for our internal network defined by Vagrantfile
+cat >> /etc/hosts <<EOL
+
+# vagrant environment nodes
+10.0.15.10  mgmt
+10.0.15.11  lb
+10.0.15.21  web1
+10.0.15.22  web2
+10.0.15.23  web3
+10.0.15.24  web4
+10.0.15.25  web5
+10.0.15.26  web6
+10.0.15.27  web7
+10.0.15.28  web8
+10.0.15.29  web9
+EOL
